3fb99d1e0f5d5f1f8a6c52ccf65e0623abacf7b2
DIma <barth0l0mew@mail.ru>
2024-05-23 21:31:33 +0300

    mongo

---
+++ Mongo/src/main/java/Mongo.java
@@ -0,0 +1,8 @@
+public class Mongo {
+    public static void main(String[] args) {
+/*
+https://www.w3schools.com/mongodb/
+https://github.com/KirillovItstep/classworks/blob/main/mongodb/employees
+ */
+    }
+}
